7230ee1f730c10c9e98beff3b55ab79057259811
masongatewood <masongatewood3@gmail.com>
2022-09-02 23:43:28 -0400

    parsed pdf and commbined sections

---
+++ scoring_tables/tables.py
@@ -0,0 +1,16 @@
+from tabula import read_pdf
+import pandas as pd
+
+file = 'WA_scoring_outdoors.pdf'
+
+def tables(pages):
+    dfs = read_pdf(file, pages=pages, multiple_tables=False)
+    return pd.DataFrame(dfs[0])
+
+middle_distance = tables('39-66')
+distance = tables('69-96')
+
+combined_distance = pd.merge(middle_distance, distance, how='inner', on=['Points', 'Points'])
+
+print(combined_distance.head())
+
